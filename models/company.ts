16a0f10104e1f97d77ae7228bb93c3b06675959a
AloneFFOfficial <chaitanyalohani175@gmail.com>
2026-02-14 11:45:13 +0530

    Add company schema model for MongoDB

---
+++ models/company.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNo: {
+        type: String,
+        required: true,
+    },
+    website: {
+        type: String,
+    },
+});
