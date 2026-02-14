ac14a478b7376f68817a0c7ba1c2e7290a204374
AloneFFOfficial <chaitanyalohani175@gmail.com>
2026-02-14 11:29:33 +0530

    Add initial API routes, database connection, and models for user and job

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDB = mongoose.connect(`${process.env.MONGO_URL}`)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+        console.log('MongoDB connection failed:', error);
+        process.exit(1);
+    });
+
+export default connectDB;
